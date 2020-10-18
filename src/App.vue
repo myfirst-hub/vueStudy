6e39315f6433e886c47f4ae716c4fa2497fb5839
sun <760172827@qq.com>
2020-10-19 07:59:02 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>TodoList</h1>
-    <input type="text" v-model="content"></input>
+    <input @keyup.enter="addTodo" type="text" v-model="content"></input>
     <button @click="addTodo">添加</button>
     <ul>
       <li v-for="(item, index) of todoData" :key="index">{{item}}</li>
@@ -21,18 +21,18 @@
         content: ''
       }
     },
-    created(){
+    created() {
       console.log('img...........', img);
-      console.log('[]...........', [1,2,3].map(item => item + 1));
+      console.log('[]...........', [1, 2, 3].map(item => item + 1));
     },
     methods: {
-      addTodo(){
-        if(this.content !== ''){
+      addTodo() {
+        if (this.content !== '') {
           this.todoData.push(this.content);
           this.content = '';
         }
         return;
-      }
+      },
     }
   }
 </script>
@@ -43,8 +43,7 @@ scoped 属性表示样式仅仅只影响当前的文件
 *//
 <style lang="stylus" scoped>
   h1
-    color: red
-    // transform: translate(100px, 100px)
+    color: red // transform: translate(100px, 100px)
   li:nth-of-type(odd)
     color: blue
 </style>
